3b3297621ec6115bdf98166c1fa82961bd33cb42
Eric Andresen <eric@andresen.codes>
2023-06-21 16:42:45 +0200

    Added models: * Ingredient * Notes * Recipe
    Updated junit.
    
    Cleaned folder structure.

---
+++ src/main/java/src/domain/Ingredient.java
@@ -0,0 +1,48 @@
+package src.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String description;
+    private Double amount;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
--- src/test/java/src/Spring5RecipeAppApplicationTests.java
+++ src/test/java/src/Spring5RecipeAppApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework;
+package src;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
